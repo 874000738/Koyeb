e249a8e41fad8f270de27c4a13f6f5f57872d382
chineseholmes <chineseholmes@gmail.com>
2022-11-27 11:57:07 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 tor &
-/xray -config /xray.json &
+/root/xray -config /root/xray.json &
 /usr/bin/p2pclient -l xichen.lou@adient.com &
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
